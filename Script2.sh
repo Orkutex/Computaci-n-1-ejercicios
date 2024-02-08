5723c11212c4cbdafa9ce9fbc2301e031625a9f8
Orkutex <159477113+Orkutex@users.noreply.github.com>
2024-02-08 13:06:39 -0600

    Update Script2.sh

--- Script2.sh
+++ Script2.sh
@@ -1,2 +1,11 @@
-#08 febrero 2024
-# Manejo de variables en Bash 
+# Script 3 - uso de variable
+# 08/02/2024
+
+raiz='ejercicio3'
+directorio1=ex1
+directorio2=ex2
+directorio3=ex3
+
+echo generando directorios
+mkdir -p $raiz/directorio1/fx1/fx2
+echo Generado con exito
