44f7f1fa05c057cbc634c3c879f5b5cba30efa49
Orkutex <159477113+Orkutex@users.noreply.github.com>
2024-02-08 13:12:00 -0600

    Add files via upload

---
+++ Script4.sh
@@ -0,0 +1,11 @@
+# Script 3 - uso de variable
+# 08/02/2024
+
+raiz='ejercicio3'
+directorio1=ex1
+directorio2=ex2
+directorio3=ex3
+
+echo generando directorios
+mkdir -p $raiz/directorio1/fx1/fx2
+echo Generado con exito
